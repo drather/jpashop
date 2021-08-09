acc6c63132f6d4c3739c783baa2f3a43a7507d4b
kks <drather@naver.com>
2021-08-10 00:05:45 +0900

    0809 10강까지 수강, LAZY, cascade, 연관관계메소드 적용

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -6,13 +6,16 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Getter@Setter
 public class Delivery {
     @Id @GeneratedValue
     private Long id;
 
-    @OneToOne(mappedBy = "delivery")
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
     @Embedded
