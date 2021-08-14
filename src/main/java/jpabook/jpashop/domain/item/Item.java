752b418a504ec6f67ff9252d4d9c97a798f62500
kks <drather@naver.com>
2021-08-14 13:35:01 +0900

    0814 16강까지 수강, 상품 엔티티 개발 및 비즈니스 로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,27 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    // 상품 주문 시, 필요한 로직은 상품 안에 넣어놓는게 좋음.
+    // DDD 시, 엔티티 자체에서 해결할 수 있는 것은 엔티티가 가지는게 좋음.(데이터를 가지고 있으므로)
+
+    //== 비즈니스 로직 ==//
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param quantity
+     */
+    public void remove(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
