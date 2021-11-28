2f73518f4ff87c4c07b29bf53744a6befd2160e4
kks <drather@naver.com>
2021-11-28 17:10:08 +0900

    7강, OrderSimpleApiController.java DTO 를 이용한 주문조회 API 작

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,12 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import jpabook.jpashop.domain.Order;
+
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne 관계에서, 성능 최적화를 어떻게 할것인가?
@@ -46,5 +52,46 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    /**
+     * v1 버젼에서, 엔티티를 직접 노출시키지 않고, DTO 로 변환해서 통신하게끔 수정.
+     * 이를 통해, 엔티티를 직접 노출시킴으로써 발생했던 문제점(엔티티-API 간 결합 증가) 해결.
+     * 그러나, 지연 로딩으로 인해 DB 에 쿼리가 너무 많이 나간다는 문제점은 아직 존재함.
+     * Order, Member, Delivery 테이블을 조회해야 하는 상황
+     *
+     * N+1 문제 발생.
+     * 첫번째 쿼리(1, 주문조회) 이후, 첫번째 쿼리의 수인 N(회원, 딜리버리 조회) 번만큼 쿼리가 추가실행 되는 문제를 말함.
+     * @return
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+
+        // ORDER 2개 조회, 쿼리 1, 누적 쿼리수 1
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        // 루프를 돌면서, Member 를 찾는 쿼리와 Delivery 를 찾는 쿼리 2개가 나감.
+        // 아래 루프를 돌고나면, Member 조회 쿼리와 Delivery 조회 쿼리 2개 * Order 수 2 해서 총 4, 누적 쿼리수 5
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address; //
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // 이 시점에서 Lazy 초기화. 영속성 컨텍스트가 id를 가지고 멤버를 찾아봄.
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // 이 시점에서 Lazy 초기화. 영속성 컨텍스트가 id를 가지고 멤버를 찾아봄.
+        }
     }
 }
