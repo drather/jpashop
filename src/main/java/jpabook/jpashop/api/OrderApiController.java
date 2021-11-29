6aa4cc6a79887e6601860b7ef7e0698d937a6986
kks <drather@naver.com>
2021-11-30 00:02:07 +0900

    11강. 엔티티를 직접 노출하지 않고, DTO 를 활용한 API 작성.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,22 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,4 +37,69 @@ public class OrderApiController {
         }
         return all;
     }
+
+    /**
+     * 이 경우, orderItems 가 null 로 출력된다.
+     * orderItems 가 엔티티이기 때문임.
+     * 따라서, orderItems 를 강제초기화 해야 함
+     *
+     * 그러나, 아직 response 에는 엔티티가 있음. (OrderItem)
+     * OrderItem 까지 DTO 로 바꿔줘야 함
+     *
+     * SQL 은 얼마나 나갈까?
+     * order 조회 2건
+     * member 조회 1번
+     * delivery 조회 1번
+     * orderItems 조회 2번.
+     * item 조회 1번
+     * 즉, 총 2 * ( 1 + 1 + 2 + 1 ) = 10번 의 쿼리가 나가게 된다. (정답인지는 모름)
+     *
+     * 이를 fetch join 을 통해 다음에 해결할 것.
+     * @return
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate; //주문시간
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+
+        private String itemName;//상품 명
+        private int orderPrice; //주문 가격
+        private int count;      //주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
 }
+
