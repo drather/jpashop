92708144a8a52dfa17188102bc956105a6bd02e8
kks <drather@naver.com>
2021-11-29 23:33:59 +0900

    10강. 엔티티를 직접 노출하는 주문 상세 조회 API 작성, 무한루프 이슈 있으나, 이후 강의에 영향 없을거같아 그냥 넘어감

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -19,7 +19,7 @@ public class JpashopApplication {
 	@Bean Hibernate5Module hibernate5Module() {
 		Hibernate5Module hibernate5Module = new Hibernate5Module();
 		// 강제로 지연로딩 해서 엔티티 정보를 가져오도록 설정 한다.
-		 hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+//		 hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
 		 return hibernate5Module;
 	}
 }
